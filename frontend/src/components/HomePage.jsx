21a9c81845cb8b2b716235f8cd4d1d2c95df8849
SnowtromAux <snowtromgs@gmail.com>
2024-01-04 17:03:18 +0200

    react setup

---
+++ frontend/src/components/HomePage.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../styles/HomePage.css';
+
+// import switcher_image_1 from '../assets/home-image-1.png';
+
+
+export default class HomeComponent extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    render() {
+        return (
+            <div>
+                <label>Hi</label>
+            </div>
+        );
+    }
+}
